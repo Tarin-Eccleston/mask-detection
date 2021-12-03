b584536cec10160b86fb4c26d249a0449fbd5103
Tarin-Eccleston <tarin.eccleston@gmail.com>
2021-12-03 17:41:55 +1300

    add examples

--- detect_mask_image.py
+++ detect_mask_image.py
@@ -73,3 +73,19 @@ for i in range(0, detections.shape[2]):
 		# has a mask or not
 		(mask, withoutMask) = model.predict(face)[0]
 
+		# determine the class label and color we'll use to draw
+		# the bounding box and text
+		label = "Mask" if mask > withoutMask else "No Mask"
+		color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+		# include the probability in the label
+		label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+		# display the label and bounding box rectangle on the output
+		# frame
+		cv2.putText(image, label, (startX, startY - 10),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+		cv2.rectangle(image, (startX, startY), (endX, endY), color, 2)
+
+# show the output image
+cv2.imshow("Output", image)
+cv2.waitKey(0)
+
